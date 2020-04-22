f2686e8bad3a0f7e9357cd3df7c51682d06d3ee9
jichen <jichen@coh.org>
2020-04-21 17:27:28 -0700

    select mutation for PCR: mutation list

---
+++ VariantCalls_mutation_list4PCR/work.sh
@@ -0,0 +1,4 @@
+ln -s ../VariantCalls_pyclone_clonevol/*.clonevol_vaf.txt ./
+ln -s ../VariantCalls/*.common_snp_info.txt ./
+ln -s ~/Projects/Breast/scRNA/97OVCZ/Alignment_Variants_WES/*.common_snp_info.txt ./
+
